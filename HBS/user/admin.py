1900bdef17f2942324e071e0224fd9ec7f80dace
martinbenny1090 <martin8086benny@gmail.com>
2021-01-20 08:31:51 +0530

    models updated

--- HBS/user/admin.py
+++ HBS/user/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Room, Booking, Cities, Popularlocations, Hotels
+from .models import Room, Booking, Cities, Popularlocations, Hotels, Room_categories
 # Register your models here.
 
 admin.site.register(Room)
@@ -7,3 +7,4 @@ admin.site.register(Booking)
 admin.site.register(Cities)
 admin.site.register(Popularlocations)
 admin.site.register(Hotels)
+admin.site.register(Room_categories)
--- HBS/user/models.py
+++ HBS/user/models.py
@@ -21,24 +21,29 @@ class Hotels(models.Model):
     h_id = models.AutoField(primary_key=True)
     location = models.ForeignKey(Popularlocations, on_delete=models.CASCADE)
     h_name = models.CharField(max_length=300, null=True, blank=True)
+    landmark = models.CharField(max_length=300, blank=True)
 
     def __str__(self):
         return f'{self.h_id} {self.h_name}'
 
+class Room_categories(models.Model):
+    ca_id = models.AutoField(primary_key=True)
+    hotel = models.ForeignKey(Hotels, on_delete=models.CASCADE)
+    categorie = models.CharField(max_length=150)
+    
+
+    def __str__(self):
+        return f'{self.hotel}. {self.categorie}'
+
+
 
 class Room(models.Model):
-    room_categorites = (
-        ('YAC',  'AC'),
-        ('NAC',  'NON-AC'),
-        ('DEL',  'DELIXE'),
-        ('KIN',  'KING'),
-        ('QUE',  'QUEEN'),
-    )
-    number = models.IntegerField()
-    category = models.CharField(max_length=3, choices=room_categorites)
     hotel = models.ForeignKey(Hotels, on_delete=models.CASCADE)
-    bed = models.IntegerField()
-    Capacity = models.IntegerField()
+    category = models.ForeignKey(Room_categories, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300, blank=True)
+    no_bed = models.IntegerField()
+    no_guest =models.IntegerField()
+    t_number = models.IntegerField()
     price = models.IntegerField()
 
     def __str__(self):
