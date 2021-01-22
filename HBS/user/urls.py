670b738976eae75412ec225702dbab1c8c594742
martinbenny1090 <martin8086benny@gmail.com>
2021-01-22 22:24:40 +0530

    search query startred

--- HBS/user/urls.py
+++ HBS/user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from user.views import  search, index
 
 urlpatterns = [
-    path("", views.index, name='index'),
+    path("", index.as_view(), name='index'),
+    path("search/", search.as_view(), name="search"),
 ]
--- HBS/user/views.py
+++ HBS/user/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render, redirect
+from django.views.generic.list import ListView , View
+from user.models import *
 
 # Create your views here.
+class index(View):
+    def get(self,request):
+        return render(request, 'index.html')
 
-def index(request):
-    return render(request, "index.html")
+    def post(self, request):
+        loc = request.POST.get('loc')
+        check_in = request.POST.get('check_in')
+        check_out = request.POST.get('check_out')
+        adult = request.POST.get('adult')
+        children =  request.POST.get('children')
+        room = request.POST.get('room')
+        print(loc)
+        print(check_in)
+        print(check_out)
+        print(adult)
+        return redirect('/')
+
+
+class search(View):
+    def get(self, request):
+        hotels = Hotels.objects.all()
+        return render(request, 'hotels_list.html', {'hotels':hotels})
