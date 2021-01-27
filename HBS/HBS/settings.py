2eaa69c5e05aa4d22ced6b53b84e84a51abe742c
martinbenny1090 <martin8086benny@gmail.com>
2021-01-27 10:12:02 +0530

    Html home page added

--- HBS/HBS/settings.py
+++ HBS/HBS/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'qm+o!upi#ku*x(*48xn#z^)u&21dmc8s0zt+4gbx=n@@k1x#xi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'user',
     'hotel',
+    'accounts',
 ]
 
 MIDDLEWARE = [
---
+++ HBS/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
+urlpatterns = [
+
+    # path('', home.as_view(), name="home"),
+
+
+]
+    
