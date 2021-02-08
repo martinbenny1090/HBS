8bf3eae079e909dd46a981bf11555e4ac0f8ec03
martinbenny1090 <martin8086benny@gmail.com>
2021-02-08 11:25:29 +0530

    adhithi app created

--- HBS/user/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Room, Popularlocations, Hotels
-# Register your models here.
-
-admin.site.register(Room)
-admin.site.register(Popularlocations)
-admin.site.register(Hotels)
