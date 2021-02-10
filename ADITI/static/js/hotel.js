cba3981f14eba78f8254711824cfb8c449c424e4
martinbenny1090 <martin8086benny@gmail.com>
2021-02-10 12:41:43 +0530

    map js added

--- ADITI/static/js/hotel.js
+++ ADITI/static/js/hotel.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
     $(".h_list").hide();
     $(".payout").hide();
     $(".payment").hide();
+    $(".b-skills").hide();
 
     $(".bm").click(function () {
         $(".b_mngmnt").show();
@@ -19,6 +20,8 @@ $(document).ready(function () {
         $(".h_list").hide();
         $(".payout").hide();
         $(".payment").hide();
+        $(".b-skills").hide();
+
     });
 
     $(".rm").click(function () {
@@ -27,14 +30,28 @@ $(document).ready(function () {
         $(".h_list").hide();
         $(".payout").hide();
         $(".payment").hide();
+        $(".b-skills").hide();
+
 
     });
+
+    $(".rp").click(function () {
+        $(".b_mngmnt").hide();
+        $(".r_mngmnt").hide();
+        $(".h_list").hide();
+        $(".payout").hide();
+        $(".payment").hide();
+        $(".b-skills").show();
+    });
+
     $(".hl").click(function () {
         $(".r_mngmnt").hide();
         $(".b_mngmnt").hide();
         $(".h_list").show();
         $(".payout").hide();
         $(".payment").hide();
+        $(".b-skills").hide();
+
 
     });
     $(".po").click(function () {
@@ -43,6 +60,8 @@ $(document).ready(function () {
         $(".h_list").hide();
         $(".payout").show();
         $(".payment").hide();
+        $(".b-skills").hide();
+
 
     });
     $(".pm").click(function () {
@@ -51,8 +70,9 @@ $(document).ready(function () {
         $(".h_list").hide();
         $(".payout").hide();
         $(".payment").show();
+        $(".b-skills").hide();
+
 
     });
-});
-
+})
 
