ede3c5d0ea86bb958346f34e00f6fa12873b212c
Mannmohan <mannmohan26@gmail.com>
2021-02-20 10:56:03 +0530

    styles updated

--- ADITI/static/js/hotel.js
+++ ADITI/static/js/hotel.js
@@ -5,3 +5,14 @@ $(document).ready(function () {
         $(".mWrapper").toggle(); 
     });
 });
+
+$(document).mouseup(function(e) 
+{
+    var container = $(".mWrapper");
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
+    {
+        container.hide();
+    }
+});
